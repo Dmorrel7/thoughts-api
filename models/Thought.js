3b48a06d8ac0f7607b203a5294a7906523dedbe2
Dalton Morrel <daltonmorrel7&@gmail.com>
2021-03-06 19:44:26 -0500

    created schema

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const ThoughtSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true
+        },
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
